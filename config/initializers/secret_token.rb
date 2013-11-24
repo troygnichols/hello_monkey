511e2180c74f5627a5dfc0d53a394f0f90e2887f
Troy Nichols <tnichols@onsitehd.com>
2013-11-24 20:20:38 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HelloMonkey::Application.config.secret_key_base = 'ccc28a93d00820a8d7dc20acaacc4c6dfdc4c3d250b8413a6f33e2336e2d8ad3c5b6cd01063d2b25d30b64e92e1ad204d31a83d6914aa964e40bc92ecfa4f445'
